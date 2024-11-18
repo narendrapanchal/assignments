0616043ab0600cd7324237b3fd38a87c3c899739
narendrapanchal <narendrapanchal020@gmail.com>
2024-11-18 14:55:11 +0530

    as

---
+++ three-d-carousel/src/components/SimpleCarousel.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./SimpleCarousel.css"
+function SimpleCarousel() {
+  return (
+    <div className="container">
+    <div className="carousel">
+      <div className="carousel__face"><span>This is something</span></div>
+      <div className="carousel__face"><span>Very special</span></div>
+      <div className="carousel__face"><span>Special is the key</span></div>
+      <div className="carousel__face"><span>For you</span></div>
+      <div className="carousel__face"><span>Just give it</span></div>
+      <div className="carousel__face"><span>A try</span></div>
+      <div className="carousel__face"><span>And see</span></div>
+      <div className="carousel__face"><span>How IT Works</span></div>
+      <div className="carousel__face"><span>Woow</span></div>
+    </div>
+  </div>
+  )
+}
+
+export default SimpleCarousel
